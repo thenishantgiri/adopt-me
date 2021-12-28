2febd7f02335709917ac9a702d278771ddd85196
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-28 20:44:57 +0530

    parcel setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-/* global React ReactDOM */
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
