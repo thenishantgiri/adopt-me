d966965171714ff5789b23e1aa79c8ecec578979
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-30 05:01:56 +0530

    added modals: portals

--- src/Details.js
+++ src/Details.js
@@ -3,9 +3,10 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
@@ -22,11 +23,20 @@ class Details extends Component {
       )
     );
   }
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  adopt = () => {
+    return (window.location = "http://bit.ly/pet-adopt");
+  };
+
   render() {
     if (this.state.loading) {
       return <h2>loading ...</h2>;
     }
-    const { name, animal, breed, city, state, description, images } =
+    const { name, animal, breed, city, state, description, images, showModal } =
       this.state;
 
     return (
@@ -38,12 +48,50 @@ class Details extends Component {
             {animal.charAt(0).toUpperCase() + animal.slice(1)} - {breed} -{" "}
             {city}, {state}
           </h2>
+
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
+
           <p>{description}</p>
+
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <ThemeContext.Consumer>
+                    {([theme]) => (
+                      <button
+                        onClick={this.adopt}
+                        style={{ backgroundColor: theme }}
+                      >
+                        Yes
+                      </button>
+                    )}
+                  </ThemeContext.Consumer>
+
+                  <ThemeContext.Consumer>
+                    {([theme]) => (
+                      <button
+                        onClick={this.toggleModal}
+                        style={{ backgroundColor: theme }}
+                      >
+                        No
+                      </button>
+                    )}
+                  </ThemeContext.Consumer>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
