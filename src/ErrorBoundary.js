f399a8a8d95afdb65f0987083c9e6dda80d4598a
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-30 03:32:44 +0530

    redirect implemented

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,8 +1,8 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
 
   static getDerivedStateFromError() {
     return { hasError: true };
@@ -11,14 +11,17 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     // log into any login monitor service - Azure Monitor, New Relic, TrackJS
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing has an error. <Link to="/">Click here</Link> to go back
-          to home page.
+          to home page or wait five seconds.
         </h2>
       );
     }
