1fc963a188a9f71e139bdbb76bb5d372bf9dbc30
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-30 02:24:02 +0530

    pet card created: using class components

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,46 @@
-const Details = () => {
-    return <h2>Details Loading...</h2>;
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+    const { name, animal, breed, city, state, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details;
+export default withRouter(Details);
