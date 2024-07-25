88d2c47a6664149a3bd6b65810191b98694eb065
thenishantgiri <nishantkrgiri@gmail.com>
2024-07-25 21:38:56 +0530

    react-router integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
