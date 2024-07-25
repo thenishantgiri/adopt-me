fe8e8e2a5442448a0d248fc758c0d9cac3216aeb
thenishantgiri <nishantkrgiri@gmail.com>
2024-07-25 22:36:23 +0530

    fetchPet: reactQuery implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/" element={<SearchParams />} />
-          <Route path="/details/:id" element={<Details />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   );
